97b9c3658f664fb110ec9b6271572e329ee5a67b
marveltaylor <marveltaylor85@gmail.com>
2023-08-01 16:12:32 -0700

    created variable for players and setPlayers with useState hook

--- src/components/AllPlayers.jsx
+++ src/components/AllPlayers.jsx
@@ -2,6 +2,7 @@ import ajaxHelpers from './API/ajaxHelpers'
 import { useState, useEffect } from 'react';
 
 export default function AllPlayers () {
+    const [players, setPlayers] = useState([])
 
     return (
         <>
