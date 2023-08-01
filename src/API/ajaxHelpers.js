e68cac49bbbc4db159f7f6451abc8cce3b2b7488
marveltaylor <marveltaylor85@gmail.com>
2023-08-01 15:54:51 -0700

    created a function with try catch block for fetchAllPlayers

---
+++ src/API/ajaxHelpers.js
@@ -0,0 +1,10 @@
+const fetchAllPlayers = async () => {
+    try {
+        const response = await fetch (`${APIURL}/players`);
+        const result = await response.json();
+        if (result.error) throw result.error;
+        return result.data.players;
+    } catch (err) {
+        console.error("Uh oh, trouble fetching players!", err);
+    }
+};
