9a679aae6800bcbaf0862f8e1d86d1360f31a312
marveltaylor <marveltaylor85@gmail.com>
2023-08-01 16:17:48 -0700

    invoked useEffect hook and passed it an empty dependency array

--- src/components/AllPlayers.jsx
+++ src/components/AllPlayers.jsx
@@ -2,7 +2,11 @@ import ajaxHelpers from './API/ajaxHelpers'
 import { useState, useEffect } from 'react';
 
 export default function AllPlayers () {
-    const [players, setPlayers] = useState([])
+    const [players, setPlayers] = useState([]);
+    
+    useEffect (() => {
+
+    }, [])
 
     return (
         <>
