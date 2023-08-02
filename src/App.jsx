464efb34c85d4232bca3579383a7dc3111dce8e0
marveltaylor <marveltaylor85@gmail.com>
2023-08-02 16:50:08 -0700

    added route for NewPlayerForm in App file

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ function App() {
     <Routes>
       <Route path ="/" element={<AllPlayers />}/>
       <Route path="/players/:id" element={<SinglePlayer />} />
+      <Route path="/" element={<NewPlayerForm />} />
     </Routes>
     </>
   );
