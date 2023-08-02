9028adf0109574e5126873a80ff454f0cafb67cc
marveltaylor <marveltaylor85@gmail.com>
2023-08-01 19:18:48 -0700

    created SinglePlayer function with try catch block

--- src/components/SinglePlayer.jsx
+++ src/components/SinglePlayer.jsx
@@ -1,6 +1,21 @@
 import { useState, useEffect } from "react";
 
 export default function SinglePlayer () {
+    const [playerID, setPlayerId] = useState({})
+    useEffect (() => {     
+        const fetchSinglePlayer = async () => {
+        try {
+            const response = await fetch (`${APIURL}/player/${id}`);
+            const result = await response.json();
+            setPlayerId(result.data.player)
+            if (result.error) throw result.error;
+        } catch (err) {
+            console.error("Uh oh, trouble fetching the player!", err);
+        }
+    };
+    
+
+    }, []);
 
     return (
         <>
