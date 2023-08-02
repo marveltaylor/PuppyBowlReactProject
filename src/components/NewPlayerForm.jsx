32a11482ddfb8655c7bf340bb1229e0e7e41771c
marveltaylor <marveltaylor85@gmail.com>
2023-08-02 15:19:31 -0700

    created newPlayer form with handleSubmit function

--- src/components/NewPlayerForm.jsx
+++ src/components/NewPlayerForm.jsx
@@ -3,8 +3,20 @@ import { useState } from "react";
 export default function NewPlayerForm () {
     const [newPlayer, setNewPlayer] = useState("");
 
+    function handleSubmit (e) {
+        e.preventDefault();
+        console.log(newPlayer);
+    }
+
     return (
         <>
+        <form onSubmit={handleSubmit}>
+            <label>New Player: {""}
+                <input value={newPlayer} onChange={(e) => {setNewPlayer(e.target.value);
+                }}>
+                </input>
+            </label>
+        </form>
         </>
     );
 }
