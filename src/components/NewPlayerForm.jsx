407b5ad7eecb5c237acdd0d4b57400dc3543e568
marveltaylor <marveltaylor85@gmail.com>
2023-08-02 16:24:28 -0700

    testing newplayerform code

--- src/components/NewPlayerForm.jsx
+++ src/components/NewPlayerForm.jsx
@@ -1,22 +1,59 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function NewPlayerForm () {
     const [newPlayer, setNewPlayer] = useState("");
+    const [name, setName] = useState({});
+    const [breed, setBreed] = useState ({});
+    const [imageUrl, setImageUrl] = useState ({});
 
-    function handleSubmit (e) {
-        e.preventDefault();
-        console.log(newPlayer);
-    }
+    useEffect (() => {
+        const addNewPlayer = async (newPlayer) => {
+            try {
+                const response = await fetch (`${APIURL}/players`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(newPlayer),
+                });
+                const result = await response.json();
+                if (result.error) throw result.error;
+                return result.data.newPlayer;
+            } catch (err) {
+                console.error('Oops, something went wrong with adding that player!', err);
+            }
+        };
+    })
+
+    // // function handleSubmit (e) {
+    // //     e.preventDefault();
+    // //     console.log(newPlayer);
+    // }
 
     return (
         <>
-        <form onSubmit={handleSubmit}>
-            <label>New Player: {""}
-                <input value={newPlayer} onChange={(e) => {setNewPlayer(e.target.value);
+        <form method="post" onSubmit={handleSubmit}>
+            <h3>Add a New Player!</h3>
+            <label>Name: {""}
+                <input value={name} onChange={(e) => {setName(e.target.value);
                 }}>
                 </input>
             </label>
-            <button type="submit">Submit</button>
+
+            <label>Breed: {""}
+                <input value={breed} onChange={(e) => {setBreed(e.target.value);
+                }}>
+                </input>
+            </label>
+
+            <label>ImageUrl: {""}
+                <input value={imageUrl} onChange={(e) => {setImageUrl(e.target.value);
+                }}>
+                </input>
+            </label>
+            
+            <button type="reset">Reset Form</button>
+            <button type="submit">Submit Form</button>
         </form>
         </>
     );
