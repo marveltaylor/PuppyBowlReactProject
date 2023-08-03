cb198ef2de846871b4cb7a5542918e1fdb4dfd5e
marveltaylor <marveltaylor85@gmail.com>
2023-08-02 23:18:12 -0700

    declared players and search variables set to useState

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,6 +1,10 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
+const [players, setPlayers] = useState([]);
+const [search, setSearch] = useState(null);
+const navigate = useNavigate();
+
 export default function SearchBar () {
 
     return (
