9629051c9944a234f0b161364f4dcdc74a147c29
marveltaylor <marveltaylor85@gmail.com>
2023-08-03 02:02:28 -0700

    testing SearchBar code

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,42 +1,52 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+// import React from 'react';
+// import { useState, useEffect } from 'react';
 
-const SearchBar = ({ onSearch }) => {
-    const [searchPup, setSearchPup] = useState("");
-    return (
-        <input type="text" placeholder="Search" onChange={(e) => onSearch(e.target.value)}/>
-    );
-}
+// const SearchBar = ({ onSearch }) => {
+//     const [searchPup, setSearchPup] = useState("");
 
-export default SearchBar;
+//     const handleSearch = (searchPlayer) => {
+//         searchPup(searchPlayer);
 
-// import { useState, useEffect } from "react";
-// import { useNavigate } from "react-router-dom";
-
-// export default function SearchBar (id) {
-// const [players, setPlayers] = useState([]);
-// const [search, setSearch] = useState("");
-// const navigate = useNavigate();
-
-//     navigate(`/view/${id}`);
-
-//     useEffect(() => {
-//         async function fetchAllPlayers() {
-//             const response = await apifetchAllPlayers();
-//             if (search) {
-//                 const filteredPlayers = response.filter((item) => item.name === search);
-//                 setPlayers(filteredPlayers);
-//             } else {
-//                 setPlayers(response);
-//             }
-//             console.log(response);
-//         }
-//         fetchAllPlayers();
-//     }, [search]);
-
+//     };
 //     return (
 //         <>
-//         <SearchBar setSearch={setSearch} />
+//         <div>
+//             <SearchBar onSearch={handleSearch} />
+//         </div>
+//         <input type="text" placeholder="Search" onChange={(e) => onSearch(e.target.value)}/>
 //         </>
 //     );
-// };
+// }
+
+// export default SearchBar;
+
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function SearchBar (id) {
+const [players, setPlayers] = useState([]);
+const [search, setSearch] = useState("");
+const navigate = useNavigate();
+
+    navigate(`/view/${id}`);
+
+    useEffect(() => {
+        async function fetchAllPlayers() {
+            const response = await fetchAllPlayers();
+            if (search) {
+                const filteredPlayers = response.filter((item) => item.name === search);
+                setPlayers(filteredPlayers);
+            } else {
+                setPlayers(response);
+            }
+            console.log(response);
+        }
+        fetchAllPlayers();
+    }, [search]);
+
+    return (
+        <>
+        <SearchBar setSearch={setSearch} />
+        </>
+    );
+};
