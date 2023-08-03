8cb6957cf8dcd6f03411198599146a1a4b8b7171
marveltaylor <marveltaylor85@gmail.com>
2023-08-03 11:49:52 -0700

    imported handleDelete function

--- src/components/DeleteButton.jsx
+++ src/components/DeleteButton.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
+import { handleDelete} from "../API/ajaxHelpers.js"
 
 export default function DeleteButton () {
     const {id} = useParams();
