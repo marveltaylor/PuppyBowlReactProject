f128b1a55cc084245cd3c7874de8148b3d09f188
marveltaylor <marveltaylor85@gmail.com>
2023-08-02 21:17:37 -0700

    imported DeleteButton into SinglePlayer.jsx

--- src/components/SinglePlayer.jsx
+++ src/components/SinglePlayer.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import DeleteButton from "./DeleteButton";
 import ajaxHelpers from '../API/ajaxHelpers';
 
 export default function SinglePlayer () {
